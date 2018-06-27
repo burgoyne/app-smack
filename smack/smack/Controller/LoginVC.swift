a59d2782c9317aa34023c17636458a4a046288b7
Andre Burgoyne <andreburgoyne@Andres-MacBook-Pro.local>
2018-06-27 16:06:47 -0400

    added login layout

---
+++ smack/smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Andre Burgoyne on 2018-06-27.
+//  Copyright © 2018 Andre Burgoyne. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smack/smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Andre Burgoyne on 2018-06-27.
+//  Copyright © 2018 Andre Burgoyne. All rights reserved.
+//
+
+import Foundation
+
+//segues
+let TO_LOGIN = "toLogin"
