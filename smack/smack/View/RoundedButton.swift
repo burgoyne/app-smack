a9c051d25dea7345a9112df16ec5dc6e956d2b93
Andre Burgoyne <andreburgoyne@Andres-MacBook-Pro.local>
2018-07-04 11:55:07 -0400

    user registration implemented - added button corner radius

---
+++ smack/smack/View/RoundedButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundedButton.swift
+//  smack
+//
+//  Created by Andre Burgoyne on 2018-07-04.
+//  Copyright © 2018 Andre Burgoyne. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
