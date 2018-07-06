652dc6bdda290dfa398528d58dd119f286f62b5d
Andre Burgoyne <andreburgoyne@Andres-MacBook-Pro.local>
2018-07-06 13:51:55 -0400

    added avatar picker for account creation

--- smack/smack/Controller/CreateAccountVC.swift
+++ smack/smack/Controller/CreateAccountVC.swift
@@ -24,6 +24,13 @@ class CreateAccountVC: UIViewController {
         super.viewDidLoad()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
+    }
+    
     @IBAction func createAccntPressed(_ sender: Any) {
         guard let name = usernameTxt.text , usernameTxt.text != "" else { return }
         guard let email = emailTxt.text , emailTxt.text != "" else { return }
@@ -46,6 +53,7 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func pickAvatarPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_AVATAR_PICKER, sender: nil)
     }
     
     @IBAction func pickBGColorPressed(_ sender: Any) {
