d75548f1dd949000750b2c4508cdb426d224ee3e
Andre Burgoyne <andreburgoyne@Andres-MacBook-Pro.local>
2018-07-11 20:51:10 -0400

    pushing for save purposes - xcode crash

---
+++ smack/smack/Controller/AddChannelVC.swift
@@ -0,0 +1,41 @@
+//
+//  AddChannelVC.swift
+//  smack
+//
+//  Created by Andre Burgoyne on 2018-07-11.
+//  Copyright © 2018 Andre Burgoyne. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+    
+    //outlets
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var chanDesc: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView() {
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedStringKey.foregroundColor : smackPurplePlaceHolder])
+        chanDesc.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedStringKey.foregroundColor : smackPurplePlaceHolder])
+    }
+    
+    @objc func closeTap(_ recognizer: UIGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+}
