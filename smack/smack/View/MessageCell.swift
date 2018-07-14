a8ea54515ba0ccdd667c325b89d8fc62d9b603f1
Andre Burgoyne <andreburgoyne@Andres-MacBook-Pro.local>
2018-07-13 22:26:17 -0400

    able to receive sent messages

---
+++ smack/smack/View/MessageCell.swift
@@ -0,0 +1,31 @@
+//
+//  MessageCell.swift
+//  smack
+//
+//  Created by Andre Burgoyne on 2018-07-13.
+//  Copyright © 2018 Andre Burgoyne. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    //outlets
+    @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timeStampLbl: UILabel!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell(message: Message) {
+        messageBodyLbl.text = message.message
+        userNameLbl.text = message.userName
+        userImg.image = UIImage(named: message.userAvatar)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+
+}
